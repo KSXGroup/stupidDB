a472f74abc7562d85a2a6d3849f78e0414b55eb8
KSXGroup <kstarxin@gmail.com>
2018-05-13 02:28:12 +0800

    add db

---
+++ DB.hpp
@@ -0,0 +1,41 @@
+#ifndef _STUPID_DB_HPP_
+#define _STUPID_DB_HPP_
+#include "BPTree.hpp"
+template<typename A, typename B>
+struct mypair{
+    A first;
+    B second;
+    mypair(A st, B nd):first(st), second(nd){}
+};
+
+template <typename D, typename K>
+class DB{
+private:
+    BPTree<K, D, std::less<K> > *tree = nullptr;
+public:
+    DB(){}
+    ~DB(){
+        if(tree) delete tree;
+        tree = nullptr;
+    }
+    void init(const char *fileName){
+        tree = new BPTree<D, K>(fileName);
+        for(int i = 1; i <= 1000; ++i) tree->insertData(i, i);
+        tree->dfs();
+    }
+    void insert(const K &Key, const D &dta){
+        tree->insertData(Key, dta);
+    }
+    void modify(const K &Key, const D &dta){
+        tree->modifyData(Key, dta);
+    }
+    mypair<D*, bool> find(const K &Key){
+        D* p = nullptr;
+        p = tree->findU(Key);
+        if(!p) return mypair<D*, bool>(0, p);
+        else return mypair<D*, bool>(1, p);
+    }
+
+};
+class multiDB{};
+#endif
